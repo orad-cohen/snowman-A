0e8fd7d882e865bcdec8dcdb564be9307a2c0689
orad-cohen <57364882+orad-cohen@users.noreply.github.com>
2021-03-09 01:56:43 +0200

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -22,18 +22,16 @@ string nospaces(string input) {
 	return input;
 }
 
-TEST_CASE("good snowman code"){
-    CHECK(nospaces(snowman(11111111))==nospaces(\n _===_\n (.,.)\n<( : )>\n ( : )\n"));
-    CHECK(nospaces(snowman(22412131))==nospaces(\n  ___ \n  ....\n\\(-..)\n (> <)>\n ( : )\n"));
-    CHECK(nospaces(snowman(12411223))==nospaces(\n _===_\n\\(-..)\n (\" \")>\n (___)\n"));
-    CHECK(nospaces(snowman(32334144))==nospaces(\n   _  \n  /_\\ \n (o.o)\n (   )>\n (   )\n"));
-    CHECK(nospaces(snowman(12341234))==nospaces(\n _===_\n (O.o)/\n<(> <)\n (   )"));
-    CHECK(nospaces(snowman(43214321))==nospaces(\n  ___ \n (_*_)\n (o_.)\n (] [)\\ \n ( : )\n"));
-    CHECK(nospaces(snowman(43432112))==nospaces(\n  ___ \n (_*_)\n\\(-_O)\n ( : )>\n (\" \")\n"));
-    CHECK(nospaces(snowman(12334212))==nospaces(\n _===_\n (O.O)/\n ( : )\n ( : )\n"));
-    CHECK(nospaces(snowman(4232212))==nospaces(\n _===_\n (o O)\n\\( : )/\n ( : )\n"));
-    CHECK(nospaces(snowman(12411222))==nospaces(\n _===_\n\\(-..)\n (\" \")>\n ( : )\n"));
-}
+  CHECK(nospaces(snowman(11111111))==nospaces("\n _===_\n (.,.)\n<( : )>\n ( : )\n"));
+    CHECK(nospaces(snowman(22412131))==nospaces("\n  ___ \n  ....\n\\(-..)\n (> <)>\n ( : )\n"));
+    CHECK(nospaces(snowman(12411223))==nospaces("\n _===_\n\\(-..)\n (\" \")>\n (___)\n"));
+    CHECK(nospaces(snowman(32334144))==nospaces("\n   _  \n  /_\\ \n (o.o)\n (   )>\n (   )\n"));
+    CHECK(nospaces(snowman(12341234))==nospaces("\n _===_\n (O.o)/\n<(> <)\n (   )"));
+    CHECK(nospaces(snowman(43214321))==nospaces("\n  ___ \n (_*_)\n (o_.)\n (] [)\\ \n ( : )\n"));
+    CHECK(nospaces(snowman(43432112))==nospaces("\n  ___ \n (_*_)\n\\(-_O)\n ( : )>\n (\" \")\n"));
+    CHECK(nospaces(snowman(12334212))==nospaces("\n _===_\n (O.O)/\n ( : )\n ( : )\n"));
+    CHECK(nospaces(snowman(4232212))==nospaces("\n _===_\n (o O)\n\\( : )/\n ( : )\n"));
+    CHECK(nospaces(snowman(12411222))==nospaces("\n _===_\n\\(-..)\n (\" \")>\n ( : )\n"));
 
 TEST_CASE("Bad snowman code"){
     CHECK_THROWS(snowman(555));
