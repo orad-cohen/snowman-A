cfef29fe1c70963d48270de415f25d2f6baf9e7d
orad cohen <orad37@gmail.com>
2021-03-07 21:55:16 -0500

    Initial commit

---
+++ Test.cpp
@@ -0,0 +1,53 @@
+#include "doctest.h"
+#include "snowman.hpp"
+using namespace ariel;
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+/**
+ * 1 _===_
+ * 2  ___ \n ....
+ * 3   _  \n /_\\
+ * 4  ___ \n (_*_)
+ * HNLRXYTB
+ **/
+
+string nospaces(string input) {
+	std::erase(input, ' ');
+	std::erase(input, '\t');
+	std::erase(input, '\n');
+	std::erase(input, '\r');
+	return input;
+}
+
+TEST_CASE("good snowman code"){
+    CHECK(snowman(11111111)=="\n _===_\n (.,.)\n<( : )>\n ( : )\n");
+    CHECK(snowman(22412131)=="\n  ___ \n  ....\n\\(-..)\n (> <)>\n ( : )\n");
+    CHECK(snowman(12411223)=="\n _===_\n\\(-..)\n (\" \")>\n (___)\n");
+    CHECK(snowman(32334144)=="\n   _  \n  /_\\ \n (o.o)\n (   )>\n (   )\n");
+    CHECK(snowman(12341234)=="\n _===_\n (O.o)/\n<(> <)\n (   )");
+    CHECK(snowman(43214321)=="\n  ___ \n (_*_)\n (o_.)\n (] [)\\ \n ( : )\n");
+    CHECK(snowman(43432112)=="\n  ___ \n (_*_)\n\\(-_O)\n ( : )>\n (\" \")\n");
+    CHECK(snowman(12334212)=="\n _===_\n (O.O)/\n ( : )\n ( : )\n");
+    CHECK(snowman(4232212)=="\n _===_\n (o O)\n\\( : )/\n ( : )\n");
+    CHECK(snowman(12411222)=="\n _===_\n\\(-..)\n (\" \")>\n ( : )\n");
+}
+
+TEST_CASE("Bad snowman code"){
+    CHECK_THROWS(snowman(555));
+    CHECK_THROWS(snowman(1234432));
+    CHECK_THROWS(snowman(11245213));
+    CHECK_THROWS(snowman(111322122));
+    CHECK_THROWS(snowman(432113));
+    CHECK_THROWS(snowman(232));
+    CHECK_THROWS(snowman(12342215));
+    CHECK_THROWS(snowman(552212341));
+    CHECK_THROWS(snowman(1));
+    CHECK_THROWS(snowman(2231563));
+    CHECK_THROWS(snowman(23213));
+
+}
+
+
---
+++ snowman.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+#include "snowman.hpp"
+
+namespace ariel{
+
+    std::string snowman(int n){
+        
+     return std::string("\n _===_\n (.,.)\n<( : )>\n ( : )\n");   
+    }
+
+}
